c316fbcf571b5461cd3970e6a1fa64f886125caf
Pierre Cabrière <pierre.cabriere@gmail.com>
2016-09-28 20:17:38 +0200

    improvments

--- src/app/horizon.service.ts
+++ src/app/horizon.service.ts
@@ -2,15 +2,22 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { HORIZON_CONFIG } from './app.config';
 
+// get horizon client service
 const Horizon = require('@horizon/client');
 
 @Injectable()
 export class HorizonService {
+  // declare private horizon var
   private _horizon: any;
 
   constructor() {
+    // connect to the horizon service
     this._horizon = Horizon(HORIZON_CONFIG);
-    this._horizon.connect();
+  }
+
+  // getter for the horizon service
+  public get horizon() {
+    return this._horizon;
   }
 
 }
